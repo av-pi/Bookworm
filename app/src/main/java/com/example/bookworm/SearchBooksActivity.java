8be52cce2fd1d2f7937d8444bce5b2ce176e7e67
akash <akash.varma0110@gmail.com>
2023-06-21 18:40:35 +0530

    API changed to Google Books API. Searched books now show in a recycler view in a dedicated activity page.

--- app/src/main/java/com/example/bookworm/SearchBooksActivity.java
+++ app/src/main/java/com/example/bookworm/SearchBooksActivity.java
@@ -2,12 +2,17 @@ package com.example.bookworm;
 
 import static android.content.ContentValues.TAG;
 
+import static com.example.bookworm.Book.BOOK_STATUS_INTERESTED;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -20,27 +25,35 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class SearchBooksActivity extends AppCompatActivity {
 
+    private RecyclerView searchResultsRecView;
     private RequestQueue requestQueue;
     private JsonObjectRequest jsonRequest;
-    private String url = "https://openlibrary.org/search.json?q=";
+    private String url = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    private ArrayList<Book> searchResultsList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_books);
+
+        searchResultsRecView = findViewById(R.id.search_results_rec_view);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            //doMySearch(query);
-            //Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
-
             searchQuery(query);
         }
+
     }
 
     private void searchQuery(String query) {
@@ -53,7 +66,48 @@ public class SearchBooksActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 //TODO: Parse response object and show results
-                Toast.makeText(SearchBooksActivity.this, "data retrieved", Toast.LENGTH_SHORT).show();
+                try {
+                    JSONArray array = response.getJSONArray("items");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject item = array.getJSONObject(i);
+
+                        // Book object fields
+                        String title = item.getJSONObject("volumeInfo").getString("title");
+                        String author = parseAuthors(item.getJSONObject("volumeInfo").getJSONArray("authors"));
+                        String imgUrl = item.getJSONObject("volumeInfo").getJSONObject("imageLinks").getString("thumbnail") + ".jpg";
+                        String bookUrl = item.getJSONObject("volumeInfo").getString("previewLink");
+                        String shortDesc = item.getJSONObject("searchInfo").getString("textSnippet");
+                        String longDesc = item.getJSONObject("volumeInfo").getString("description");
+
+
+                        Book book = new Book(title, author, imgUrl, bookUrl, shortDesc, longDesc);
+                        searchResultsList.add(book);
+
+                    }
+                    BooksRecViewAdapter adapter = new BooksRecViewAdapter(SearchBooksActivity.this);
+                    searchResultsRecView.setAdapter(adapter);
+
+                    searchResultsRecView.setLayoutManager(new LinearLayoutManager(SearchBooksActivity.this));
+
+                    adapter.setBookList(searchResultsList);
+                    //test.setText(response.getString("title"));
+                } catch (JSONException e) {
+                    Toast.makeText(SearchBooksActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            private String parseAuthors(JSONArray array) {
+                String authors = "";
+                for (int i = 0; i < array.length(); i++) {
+                    try {
+                        authors = authors + array.get(i);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                    authors = authors + "\n";
+                }
+                return authors;
             }
         }, new Response.ErrorListener() {
             @Override
